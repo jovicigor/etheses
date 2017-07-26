50b86cac13c128e9a1ec80d18fad446ce8964d2e
igor <jovic.i24@gmail.com>
2017-07-26 11:08:49 +0200

    switch to using domain model instead and remove model from persistance

--- EThesesApp/domain/src/main/java/rs/fon/pzr/model/CourseEntity.java
+++ EThesesApp/domain/src/main/java/rs/fon/pzr/model/CourseEntity.java
@@ -1,57 +1,69 @@
 package rs.fon.pzr.model;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "course")
 public class CourseEntity {
 
-	@Id
-	@Column(name = "course_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "course_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-	@Column(name = "name")
+    @Column(name = "name")
     private String name;
-	
-	@Column(name = "name_short")
+
+    @Column(name = "name_short")
     private String nameShort;
-	
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "course_studies", joinColumns = { @JoinColumn(name = "course_id") }, inverseJoinColumns = { @JoinColumn(name = "studies_id") })
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "course_studies", joinColumns = {@JoinColumn(name = "course_id")}, inverseJoinColumns = {@JoinColumn(name = "studies_id")})
     private Set<StudiesEntity> studies = new HashSet<>();
-	
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    protected CourseEntity() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public CourseEntity(String name, String nameShort, Set<StudiesEntity> studies) {
+        this.name = name;
+        this.nameShort = nameShort;
+        this.studies = studies;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}	
+    public Long getId() {
+        return id;
+    }
 
-	public String getNameShort() {
-		return nameShort;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setNameShort(String nameShort) {
-		this.nameShort = nameShort;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Set<StudiesEntity> getStudies() {
-		return studies;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setStudies(Set<StudiesEntity> studies) {
-		this.studies = studies;
-	}	
+    public String getNameShort() {
+        return nameShort;
+    }
+
+    public void setNameShort(String nameShort) {
+        this.nameShort = nameShort;
+    }
+
+    public Collection<StudiesEntity> getStudies() {
+        return new HashSet<>(studies);
+    }
+
+    public void updateStudies(Collection<StudiesEntity> studies) {
+        this.studies = studies.stream()
+                .collect(Collectors.toSet());
+    }
 }
