a3391586f1125dcd0def4aa602f0ec88cc81400f
= <jovic.i24@gmail.com>
2017-07-31 20:35:53 +0200

    remove pass encoder

--- EThesesApp/service/src/main/java/rs/fon/pzr/core/service/UserServiceImpl.java
+++ EThesesApp/service/src/main/java/rs/fon/pzr/core/service/UserServiceImpl.java
@@ -20,13 +20,9 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    private final BCryptPasswordEncoder passwordEncoder;
-
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        passwordEncoder = new BCryptPasswordEncoder();
-
     }
 
     @Override
